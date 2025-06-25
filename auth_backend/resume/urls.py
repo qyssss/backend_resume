5525bc1dd6a19a265656e14b718fe418bf4e9238
qyssss <26544143@qq.com>
2025-06-25 22:40:48 +0800

    fix optimize

--- auth_backend/resume/urls.py
+++ auth_backend/resume/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     path('get', GetResumeView.as_view(), name='get-resume'),
     path('delete', DeleteResumeView.as_view(), name='delete-resume'),
     path('update', UpdateResumeView.as_view(), name='update-resume'),
-    path('optimize', OptimizeResumeView.as_view(), name='optimize-resume'),
+    path('optimize/', OptimizeResumeView.as_view()),  # POST 创建任务
+    path('optimize/<int:task_id>/', OptimizeResumeView.as_view()),  # GET 查询任务
     path('photoUpload', ResumePhotoUploadAPIView.as_view(), name='resume-photo-upload'),
 ]
